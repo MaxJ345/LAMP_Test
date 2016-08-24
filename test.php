095776563758ad847ddae981965b8a23181aa2f2
MaxJ345 <maxj345@yahoo.com>
2016-08-23 21:16:02 -0700

    Tests first test of PHP-cURL.

--- test.php
+++ test.php
@@ -2,13 +2,10 @@
 
 <?php
 
-$API_KEY = "ca8275be-cfcf-4499-b9d9-829ba12ca9b3";
-$URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/GeneticMaterial?api_key=" + $API_KEY;
-
 $ch = curl_init();
 //$fp = fopen("example_homepage.txt", "w");
 
-curl_setopt($ch, CURLOPT_URL, $URL);
+curl_setopt($ch, CURLOPT_URL, https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/GeneticMaterial?api_key=ca8275be-cfcf-4499-b9d9-829ba12ca9b3);
 //curl_setopt($ch, CURLOPT_FILE, $fp);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
