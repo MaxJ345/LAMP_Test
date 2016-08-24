01ab13af72e20fb1f2f44ab784533d37ccf0b27a
MaxJ345 <maxj345@yahoo.com>
2016-08-23 21:02:17 -0700

    Tests first test of PHP-cURL.

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <?php
 
 $API_KEY = "ca8275be-cfcf-4499-b9d9-829ba12ca9b3";
-$URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/BoundSuccess?api_key=" + $API_KEY;
+$URL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/GeneticMaterial?api_key=" + $API_KEY;
 
 $ch = curl_init($URL);
 //$fp = fopen("example_homepage.txt", "w");
@@ -14,7 +14,7 @@ curl_setopt($ch, CURLOPT_HEADER, 0);
 
 $output = curl_exec($ch);
 
-echo $output;
+//echo $output;
 
 curl_close($ch);
 //fclose($fp);
