496e7eaea0bb7e70bf4939495734db032301c43a
MaxJ345 <maxj345@yahoo.com>
2016-08-24 19:12:56 -0700

    Tests first test of PHP-cURL.

--- test.php
+++ test.php
@@ -10,6 +10,10 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 
 echo $result;
+echo "\n\r";
+echo json_decode($result);
+echo "\n\r";
+print_r(json_decode($result));
 
 curl_close($ch);
 
